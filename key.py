d6ca94ebc7c7250e5deb1e494f91c80bf662b431
mizuki-boc <mizuki0842boc@icloud.com>
2020-09-11 12:44:39 +0900

    deployed to heroku

--- key.py
+++ key.py
@@ -1,1 +1,7 @@
-SECRET_KEY = "jguijcbierbubwjcvev"
+import random, string
+
+def randomname(n):
+   randlst = [random.choice(string.ascii_letters + string.digits) for i in range(n)]
+   return ''.join(randlst)
+
+SECRET_KEY = randomname(100)
